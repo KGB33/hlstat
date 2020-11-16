806fa71efd3bf306111876ffdffcf0e5f25c2b14
KGB33 <github@bassingthwaite.org>
2020-11-16 14:35:57 -0700

    added stubs

--- cli/workers.go
+++ cli/workers.go
@@ -3,3 +3,19 @@ package cli
 func pingRouter() string {
 	return "pingRouter is WIP"
 }
+
+func pingGateway() string {
+	return "pingGateway is WIP"
+}
+
+func pingDNS() string {
+	return "pingDNS is WIP"
+}
+
+func pingDiscordBot() string {
+	return "pingDiscordBot is WIP"
+}
+
+func pingRaspberryPi() string {
+	return "pingRaspberryPi is WIP"
+}
