097638d59b5dd9a1da32c69d9a47f43e6c4f7a49
KGB33 <github@bassingthwaite.org>
2020-11-18 16:11:28 -0700

    added colors to output

--- cli/format.go
+++ cli/format.go
@@ -2,8 +2,29 @@ package cli
 
 import "fmt"
 
+// \033[38;2;<r>;<g>;<b>m -- forground
+const CHECK = "\xE2\x9C\x94"
+const WARN = "\xE2\x9A\xA0"
+const CROSS = "\xF0\x9D\x97\xAB"
+const CLEAR = "\033[0m"
+const RED = "\033[38;2;255;0;0m" + CROSS + CLEAR
+const YELLOW = "\033[38;2;217;158;1m" + WARN + CLEAR
+const GREEN = "\033[38;2;0;255;0m" + CHECK + CLEAR
+
+const HEADER = "---------- Home Lab Stats ----------"
+
 func defaultFormat(input map[string]StatusResponse)  {
+	var color string
+	fmt.Println(HEADER)
 	for key,val := range input {
-		fmt.Println(key, val)
+		switch val.status {
+		case "OK":
+			color = GREEN
+		case "WARN":
+			color = YELLOW
+		case "FAIL":
+			color = RED
+		}
+		fmt.Printf("   %s %s\n", color, key)
 	}
 }
